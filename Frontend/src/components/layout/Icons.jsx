3c8b62156b8c090fcdab597b8dc0900b8b629dab
PARAPOPOVICH <139543918+KOSFin@users.noreply.github.com>
2025-11-14 00:39:07 +0500

    fix

--- Frontend/src/components/layout/Icons.jsx
+++ Frontend/src/components/layout/Icons.jsx
@@ -7,13 +7,9 @@ import {
   FiUser,
 } from 'react-icons/fi';
 
-const withIcon = (IconComponent) => ({ size = 18, ...props }) => (
-  <IconComponent size={size} {...props} focusable="false" aria-hidden="true" />
-);
-
-export const IconSun = withIcon(FiSun);
-export const IconMoon = withIcon(FiMoon);
-export const IconBell = withIcon(FiBell);
-export const IconUser = withIcon(FiUser);
-export const IconLogout = withIcon(FiLogOut);
-export const IconCheckCircle = withIcon(FiCheckCircle);
+export const IconSun = (props) => <FiSun {...props} />;
+export const IconMoon = (props) => <FiMoon {...props} />;
+export const IconBell = (props) => <FiBell {...props} />;
+export const IconUser = (props) => <FiUser {...props} />;
+export const IconLogout = (props) => <FiLogOut {...props} />;
+export const IconCheckCircle = (props) => <FiCheckCircle {...props} />;
